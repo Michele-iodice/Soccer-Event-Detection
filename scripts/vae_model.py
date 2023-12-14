f16ddce9184db636be98a728604a75faa0e0a54d
Michele Iodice <M.iodice21@studenti.unisa.it>
2023-12-14 01:10:01 +0100

    build projec

---
+++ scripts/vae_model.py
@@ -0,0 +1,127 @@
+
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+import matplotlib.pyplot as plt
+
+# Encoder
+encoder_inputs = keras.Input(shape=(28, 28, 1))
+x = layers.Conv2D(16, (3, 3), padding="same")(encoder_inputs)
+x = layers.BatchNormalization()(x)
+x = layers.ReLU()(x)
+x = layers.MaxPooling2D((2, 2))(x)
+
+x = layers.Conv2D(32, (3, 3), padding="same")(x)
+x = layers.BatchNormalization()(x)
+x = layers.ReLU()(x)
+x = layers.MaxPooling2D((2, 2))(x)
+
+x = layers.Conv2D(64, (3, 3), padding="same")(x)
+x = layers.BatchNormalization()(x)
+x = layers.ReLU()(x)
+x = layers.MaxPooling2D((2, 2))(x)
+
+x = layers.Conv2D(128, (3, 3), padding="same")(x)
+x = layers.BatchNormalization()(x)
+x = layers.ReLU()(x)
+x = layers.MaxPooling2D((2, 2))(x)
+
+# Flatten for fully connected layers
+x = layers.Flatten()(x)
+x = layers.Dense(1024, activation="relu")(x)
+
+# Define the parameters for the latent space
+z_mean = layers.Dense(512, name="z_mean")(x)
+z_log_var = layers.Dense(512, name="z_log_var")(x)
+
+
+# Sampling function
+def sampling(args):
+    z_means, z_log_vars = args
+    batch = tf.shape(z_means)[0]
+    dim = tf.shape(z_means)[1]
+    epsilon = tf.keras.backend.random_normal(shape=(batch, dim))
+    z_out = z_means + tf.exp(0.5 * z_log_vars) * epsilon
+
+    # KL Divergence regularization term
+    zkl_loss = -0.5 * tf.reduce_sum(1 + z_log_vars - tf.square(z_means) - tf.exp(z_log_vars), axis=-1)
+
+    return z_out - zkl_loss
+
+
+# Sampling layer
+
+z = layers.Lambda(sampling, output_shape=(512,), name="z")([z_mean, z_log_var])
+
+# Decoder
+decoder_inputs = layers.Input(shape=(512,))
+x = layers.Dense(1024, activation="relu")(decoder_inputs)
+x = layers.Reshape((2, 2, 2, 64))(x)
+
+x = layers.UpSampling3D((2, 2, 2))(x)
+x = layers.Conv2DTranspose(128, (3, 3), padding="same")(x)
+x = layers.BatchNormalization()(x)
+x = layers.ReLU()(x)
+
+x = layers.UpSampling2D((2, 2))(x)
+x = layers.Conv2DTranspose(64, (3, 3), padding="same")(x)
+x = layers.BatchNormalization()(x)
+x = layers.ReLU()(x)
+
+x = layers.UpSampling2D((2, 2))(x)
+x = layers.Conv2DTranspose(32, (3, 3), padding="same")(x)
+x = layers.BatchNormalization()(x)
+x = layers.ReLU()(x)
+
+x = layers.UpSampling2D((2, 2))(x)
+x = layers.Conv2DTranspose(16, (3, 3), padding="same")(x)
+x = layers.BatchNormalization()(x)
+x = layers.ReLU()(x)
+
+decoder_outputs = layers.Conv2DTranspose(1, (3, 3), activation="sigmoid", padding="same")(x)
+
+# Define the VAE model
+vae = keras.Model(encoder_inputs, decoder_outputs, name="vae")
+
+# KL Divergence loss
+kl_loss = -0.5 * tf.reduce_sum(1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var), axis=-1)
+
+# Reconstruction loss
+reconstruction_loss = -tf.reduce_sum(encoder_inputs * tf.math.log(decoder_outputs + 1e-10) + (1 - encoder_inputs) * tf.math.log(1 - decoder_outputs + 1e-10), axis=-1)
+
+# Total loss
+vae_loss = tf.reduce_mean(reconstruction_loss + kl_loss)
+
+# Compile the model
+vae.compile(optimizer='adam', loss=vae_loss, metrics=['loss'])
+
+# Display the model summary
+vae.summary()
+
+# Train the VAE model with your data
+# vae.fit(train_data, epochs=epochs, batch_size=batch_size, validation_data=(val_data, None))
+
+epochs = 10
+batch_size = 64
+x_train=None
+x_val=None
+x_test=None
+
+history = vae.fit(x_train, x_train,
+                  epochs=epochs,
+                  batch_size=batch_size,
+                  validation_data=(x_val, x_val))
+
+# Evaluate the model on the test set
+eval_result = vae.evaluate(x_test, x_test, batch_size=batch_size)
+print("Test Loss:", eval_result)
+
+vae.save("models/vae/vae_model.h5")
+
+# Plot training history
+plt.plot(history.history['loss'], label='Training Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
